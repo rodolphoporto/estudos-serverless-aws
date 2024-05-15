aa44320a54da7aa07bd3e5b5cadb96d7a6f5b2b9
Rodolpho Porto <rodolpho.porto@gmail.com>
2024-05-15 15:42:02 -0300

    Introdução ao Serverless framework

---
+++ demo03-serverless-framework/run.sh
@@ -0,0 +1,17 @@
+# instalar
+npm i -g serverless@3.16.0
+
+# sls ou serveless para inicializar
+sls
+# escolhi o HTTP API TEMPLATE
+# nao usei org
+# Deploys
+
+# sempre que mudou o código, usa o
+sls deploy
+
+# traz os endereços e as informações sobre as funções
+sls info
+
+# invokar local
+sls invoke local -f api 
