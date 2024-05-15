5908682c18cbd9ea03267a25125a24706a9f5d43
Rodolpho Porto <rodolpho.porto@gmail.com>
2024-05-15 14:26:27 -0300

    OpÇão para pegar o POLICY_ARN

--- demo01-lambda-sem-framework/run.sh
+++ demo01-lambda-sem-framework/run.sh
@@ -1,10 +1,20 @@
 # 1o passo criar o arquivo de politicas de segurança
 # 2o criar role de segurança na AWS
 
+aws --version
+
+ROLE_NAME=$ROLE_NAME
+NODEJS_VERSION=nodejs16.x
+FUNCTION_NAME=hello-cli
+
 aws iam create-role \
-    --role-name lambda-exemplo \
+    --role-name $ROLE_NAME \
     --assume-role-policy-document file://politicas.json \
     | tee logs/role.log
+
+cat logs/role.log | jq .Role.Arn
+cat logs/role.log | jq .Role.Arn | tr -d "'"
+POLICY_ARN=$(cat logs/role.log | jq .Role.Arn)
 
 # 3o criar arquivo com conteúdo da função lambda
 zip function.zip index.js
@@ -14,7 +24,7 @@ aws lambda create-function \
     --zip-file fileb://function.zip \
     --handler index.handler \
     --runtime nodejs18.x \
-    --role arn:aws:iam::1234567890:role/lambda-exemplo \
+    --role arn:aws:iam::1234567890:role/$ROLE_NAME \
     | tee logs/lambda-create.log
 
 #4o invoke lambda!
@@ -45,4 +55,4 @@ aws lambda delete-function \
 
 # remover role
 aws iam delete-role \
-    --role-name lambda-exemplo
+    --role-name $ROLE_NAME
